d2ce6c04b34d784f9973cef5b64148eab0fb0d04
Denis <64834314+sanddwich@users.noreply.github.com>
2021-09-21 09:51:23 +0400

    123213

--- src/Layouts/MainLayout/MainHeader/MainHeader.tsx
+++ src/Layouts/MainLayout/MainHeader/MainHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row } from 'react-bootstrap'
 import './MainHeader.scss'
 
 interface MainHeaderProps {}
@@ -7,12 +7,29 @@ interface MainHeaderProps {}
 interface MainHeaderState {}
 
 class MainHeader extends React.Component<MainHeaderProps, MainHeaderState> {
-  
-
   render() {
     return (
       <Container fluid className="MainHeader p-0">
-        <h1>MainHeader</h1>        
+        <Container className="MainHeader__cont">
+          <Row className="MainHeader__Row justify-content-between align-items-center m-0">
+            <div className="MainHeader__logoNav d-flex justify-content-start">
+              <div className="MainHeader__logo">
+                <img src="/img/svg/logo.svg" alt="" />
+              </div>
+              <div className="MainHeader__nav d-none d-md-flex justifu-content-start align-items-center">
+                <div className="MainHeader__navEl">Преимущества</div>
+                <div className="MainHeader__navEl">О продукте</div>
+                <div className="MainHeader__navEl">Оптовикам</div>
+                <div className="MainHeader__navEl">Оптовикам</div>
+                <div className="MainHeader__navEl">Каталог</div>
+              </div>
+            </div>
+            <div className="MainHeader__phone d-block">
+              <div className="MainHeader__phone">+7 (929) 925-00-08</div>
+              <div className="MainHeader__phoneCallback">обратный звонок</div>
+            </div>
+          </Row>
+        </Container>
       </Container>
     )
   }
