844a0af718ff404bff98b153ad1d2c708a5ed4aa
denristun <58968932+denristun@users.noreply.github.com>
2021-09-22 22:08:02 +0400

    Update Contacts.tsx

--- src/Layouts/MainLayout/Pages/Components/Contacts/Contacts.tsx
+++ src/Layouts/MainLayout/Pages/Components/Contacts/Contacts.tsx
@@ -12,12 +12,14 @@ class Contacts extends React.Component<ContactsProps, ContactsState> {
     return (
       <Container className="Contacts ">
         <div className="Contacts__block1">
-          <div className="Contacts__question col-md-6">
-            <h1>Готовы оформить заказ или остались вопросы?</h1>
-            <p>Оставьте заявку. Мы свяжемся с вами и ответим на все возникшие вопросы</p>
-          </div>
-          <div className="Contacts__form col-md-6">
-            <ButtonComponent>Отправить заявку</ButtonComponent>
+          <div className="row">
+            <div className="Contacts__question col-md-6">
+              <h1>Готовы оформить заказ или остались вопросы?</h1>
+              <p>Оставьте заявку. Мы свяжемся с вами и ответим на все возникшие вопросы</p>
+            </div>
+            <div className="Contacts__form col-md-6">
+              <ButtonComponent>Отправить заявку</ButtonComponent>
+            </div>
           </div>
         </div>
       </Container>
