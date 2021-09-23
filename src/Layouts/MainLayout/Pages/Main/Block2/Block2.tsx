6ceac64efbed7b79744454284397da84069a2b03
Denis <64834314+sanddwich@users.noreply.github.com>
2021-09-23 09:54:04 +0400

    213213

--- src/Layouts/MainLayout/Pages/Main/Block2/Block2.tsx
+++ src/Layouts/MainLayout/Pages/Main/Block2/Block2.tsx
@@ -19,14 +19,65 @@ const Block2 = (props: Block2Props) => {
           <h1>AIRMIX – это</h1>
         </Row>
         <Row className="Block2__mobRow m-0 justify-content-center">
-          <Col className="Block2__mobCol" style={{
-            backgroundImage: 'url("/img/2-mob-1-min.jpg")'
-          }}>
+          <Col
+            className="Block2__mobCol"
+            style={{
+              backgroundImage: 'url("/img/2-mob-1-min.jpg")',
+            }}
+          >
             <div className="Block2__qblock d-flex justify-content-center">
               <div className="Block2__qblock1">
                 <ToggleMessageEl isOpen={true}>
                   <h4>Гипоаллергенный мундштук</h4>
                   <p>растительный глицерин , никотин 2% , пищевой пропилен-гликоль и натуральные ароматизаторы</p>
+                </ToggleMessageEl>
+              </div>
+            </div>
+            <div className="Block2__qblock d-flex justify-content-center">
+              <div className="Block2__qblock2">
+                <ToggleMessageEl isOpen={false}>
+                  <h4>Фильтр</h4>
+                  <p>Блокирует попадание жидкости в пололость рта</p>
+                </ToggleMessageEl>
+              </div>
+            </div>
+            <div className="Block2__qblock d-flex justify-content-center">
+              <div className="Block2__qblock3">
+                <ToggleMessageEl isOpen={false}>
+                  <h4>Содержит брендовую высококачественную ароматизированную жидкость</h4>
+                  <p>объёмом 3,5 мл</p>
+                </ToggleMessageEl>
+              </div>
+            </div>
+            <div className="Block2__qblock d-flex justify-content-center">
+              <div className="Block2__qblock4">
+                <ToggleMessageEl isOpen={false}>
+                  <h4>Прочный корпус</h4>
+                  <p>из нержавеющей стали</p>
+                </ToggleMessageEl>
+              </div>
+            </div>
+            <div className="Block2__qblock d-flex justify-content-center">
+              <div className="Block2__qblock5">
+                <ToggleMessageEl isOpen={false}>
+                  <h4>Надежный высокомощный аккумулятор</h4>
+                  <p>объёмом 550 mAh</p>
+                </ToggleMessageEl>
+              </div>
+            </div>
+            <div className="Block2__qblock d-flex justify-content-center">
+              <div className="Block2__qblock6">
+                <ToggleMessageEl isOpen={false}>
+                  <h4>Тяга</h4>
+                  <p>Мягкая тяга делает курение комфортным</p>
+                </ToggleMessageEl>
+              </div>
+            </div>
+            <div className="Block2__qblock d-flex justify-content-center">
+              <div className="Block2__qblock7">
+                <ToggleMessageEl isOpen={false}>
+                  <h4>Подача воздуха</h4>
+                  <p>Светодионый индикатор подачи воздуха в устройство</p>
                 </ToggleMessageEl>
               </div>
             </div>
--- src/SharedComponents/ToggleMessageEl/ToggleMessageEl.tsx
+++ src/SharedComponents/ToggleMessageEl/ToggleMessageEl.tsx
@@ -9,24 +9,24 @@ interface ToggleMessageElProps {
 }
 
 export default function ToggleMessageEl(props: ToggleMessageElProps) {
-  const [isOpen, setIsOpen] = useState<Boolean>(props.isOpen)
+  const [isOpen, setIsOpen] = useState<boolean>(props.isOpen)
 
   return (
-    <Container fluid className="ToggleMessageEl p-0">
-      <Row className="ToggleMessageEl__Row m-0 justify-content-center">
-        <div className="ToggleMessageEl__popupCont">
-          <OutsideClickHandler onOutsideClick={() => setIsOpen(false)}>
-            {isOpen && <div className="ToggleMessageEl__popup">{props.children}</div>}
-          </OutsideClickHandler>
-        </div>
-      </Row>
-      <Row className="ToggleMessageEl__action m-0 justify-content-center">
-        {isOpen ? (
-          <img src="/img/svg/closeCircle.svg" alt="" />
-        ) : (
-          <img src="/img/svg/openCircle.svg" alt="" onClick={() => setIsOpen(true)} />
-        )}
-      </Row>
-    </Container>
+    <OutsideClickHandler onOutsideClick={() => setIsOpen(false)}>
+      <Container fluid className="ToggleMessageEl p-0">
+        <Row className="ToggleMessageEl__Row m-0 justify-content-center">
+          <div className="ToggleMessageEl__popupCont">
+            {isOpen && <div className="ToggleMessageEl__popup animated animate__fadeInUp">{props.children}</div>}
+          </div>
+        </Row>
+        <Row className="ToggleMessageEl__action m-0 justify-content-center">
+          {isOpen ? (
+            <img src="/img/svg/closeCircle.svg" alt="" onMouseDown={() => setIsOpen(false)} />
+          ) : (
+            <img src="/img/svg/openCircle.svg" alt="" onMouseDown={() => setIsOpen(true)} />
+          )}
+        </Row>
+      </Container>
+    </OutsideClickHandler>
   )
 }
